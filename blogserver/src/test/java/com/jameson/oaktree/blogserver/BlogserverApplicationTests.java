7351009eff0d211daac8c88ea978303968117547
Lucifer <zoew1942spe@live.com>
2021-12-11 11:30:43 +0800

    Init blogserver.

---
+++ blogserver/src/test/java/com/jameson/oaktree/blogserver/BlogserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameson.oaktree.blogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
