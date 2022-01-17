ccee3fa1bd7d4a3fc17fefd520bd7176d4f814b9
zhangshu <zhangshu@huya.com>
2022-01-17 16:18:36 +0800

    增加 common 的类

---
+++ common/src/main/java/com/jameson/oaktree/common/entity/BaseEntity.java
@@ -0,0 +1,71 @@
+package com.jameson.oaktree.common.entity;
+
+import java.util.Date;
+
+/**
+ * @author zhangshu
+ * @date 2022-01-17 16:09
+ */
+public class BaseEntity {
+
+    private Long id;
+
+    private Date createDate;
+
+    private String createBy;
+
+    private Date modifyDate;
+
+    private String modifyBy;
+
+    public BaseEntity() {
+    }
+
+    public BaseEntity(Long id, Date createDate, String createBy, Date modifyDate, String modifyBy) {
+        this.id = id;
+        this.createDate = createDate;
+        this.createBy = createBy;
+        this.modifyDate = modifyDate;
+        this.modifyBy = modifyBy;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public String getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(String modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+}
---
+++ common/src/main/java/com/jameson/oaktree/common/model/BaseModel.java
@@ -0,0 +1,42 @@
+package com.jameson.oaktree.common.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * @author zhangshu
+ * @date 2022-01-17 10:29
+ */
+public class BaseModel {
+
+    protected static Logger logger = LoggerFactory.getLogger(BaseModel.class);
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public String toJson() {
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            logger.error("to JSON error : {}", e.getMessage());
+        }
+        return null;
+    }
+
+    public static <T> T fromJson(String json, Class<T> type) {
+        try {
+            return mapper.readValue(json, type);
+        } catch (IOException e) {
+            logger.error("{} from JSON error : {}", json, e);
+            throw new RuntimeException("JSON 格式不正确或字段存在偏差.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return toJson();
+    }
+}
